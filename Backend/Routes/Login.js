6dad9be4c87eff0b549ce6fac63c2599d4fb28b7
neuervinch <npandian515@gmail.com>
2024-12-15 16:00:18 +0530

    semi

--- Backend/Routes/Login.js
+++ Backend/Routes/Login.js
@@ -13,15 +13,22 @@ const router= express.Router();
 //    so we store their details to
 //   login before in the DB
 
-router.post('./student/login',async (res,req) =>{
+router.post('/student/login',async (res,req) =>{
  const {rollno , dob} = req.body;
  
- const student = await Student.findOne({rollno , dob});
- if(!student){
+ const student = await Student.findOne({rollno});
+ if(!student || new Date(student.dob).getTime !== new Date(dob).getTime){
     return res.status(400).json({message : "Invalid Rollno or Dob"});
     }
-    const tokken = 
+    const token = jwt.verify({id: student._id}, jwt_secret_key,{expiresIn : '1h'})
+  res.json({token});
+});
 
+router.post('/Teacher/login',AuthToken,async (res,req) =>{
+    const {username, paswword} = req.body
+
+     const Teacher = await teacher.findOne({username});
+     if(!Teacher ||hh ){}
 
 })
 
