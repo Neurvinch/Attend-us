a9a2962700a3aa96d87821761a2e49c088b82a5f
Naveen pandian <npandian515@gmail.com>
2024-12-22 11:13:11 +0530

    attendance ficd

--- Backend/Routes/Attendance.js
+++ Backend/Routes/Attendance.js
@@ -5,5 +5,27 @@ const Attendance = require('../Models/Attendance');
 
 
 
-router.get('/:studentId')
+router.get('/:studentId',AuthenticateToken,async(res,req) =>{
+
+    try {
+        const attendanceStats = await Attendance.find({studentId: req.params.studentId});
+        res.json(attendanceStats);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.post('/mark',AuthenticateToken,async(res,req) =>{
+const{studentId,date,status} = req.body
+    try {
+         const newAttendance = await new Attendance({studentId,date,status});
+         await newAttendance.save();
+         res.json({message:"attendance marked sucessfully "});        
+    } catch (error) {
+         res.status(500).json({ message: error.message });
+    }
+});
 // ToDo:: more
+
+
+module.exports = router;
--- Backend/app.js
+++ Backend/app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const AuthRoutes = require('../Backend/Routes/Login');
 const TTRoutes = require('../Backend/Routes/TimeTable');
+const atdRoutes = require('../Backend/Routes/Attendance');
 // connect toth emongoDB
 
  const app = express();
  app.use(express.json());
 
  app.use('/apiV1',AuthRoutes);
-app.use('/api/timetable', TTRoutes);
-
+app.use('/apiV1/timetable', TTRoutes);
+app.use('/apiV1/attendance',atdRoutes);
  ap.listen(5000,()=>{
     console.log('server is running nov!')   
  });
